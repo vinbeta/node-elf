c5ef0b20f8af20359c4fa7802609eb897b577af1
vinbeta <vinbeta2@hotmail.com>
2015-12-04 20:24:13 -0800

    modify meadowlark for views & layouts.

--- meadowlark.js
+++ meadowlark.js
@@ -2,19 +2,32 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+
 app.set('port',process.env.PORT || 3000);
 
-app.use(function(req,res){
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not Found');
+app.get('/',function(req,res){
+	res.render('home');
 });
 
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
+// for 404 page
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
+
+// for 500 page
 app.use(function(err,req,res,next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'),function(){
